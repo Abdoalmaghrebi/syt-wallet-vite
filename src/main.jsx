17fc2f618f20a556c26993b2f20258d125432590
Abdullrahman almaghrabi <abdprincelove18@gmail.com>
2026-03-10 22:43:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WebAppProvider } from '@vkruglikov/react-telegram-web-app'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WebAppProvider>
+      <App />
+    </WebAppProvider>
   </React.StrictMode>,
 )
